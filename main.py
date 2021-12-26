cbc267344e57673a839f3848cde479c20258103a
VG08 <71305756+VG08@users.noreply.github.com>
2021-12-26 16:41:26 +0530

    remove fake db used for testing

--- main.py
+++ main.py
@@ -16,16 +16,6 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-fake_users_db = {
-    "johndoe": {
-        "username": "johndoe",
-        "full_name": "John Doe",
-        "email": "johndoe@example.com",
-        "hashed_password": "$2b$12$EixZaYVK1fsbw1ZfbX3OXePaWxn96p36WQoeG6Lruj3vjPGga31lW",
-        "disabled": False,
-    }
-}
-
 
 # Dependency
 def get_db():
@@ -34,6 +24,7 @@ def get_db():
         yield db
     finally:
         db.close()
+
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
@@ -48,8 +39,6 @@ def verify_password(plain_password, hashed_password):
 
 def get_password_hash(password):
     return pwd_context.hash(password)
-
-
 
 
 def authenticate_user(db, username: str, password: str):
